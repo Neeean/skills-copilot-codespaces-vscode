98032b463960f3a7410064c2de25d08ca80f3fb9
Neeean <87840396+Neeean@users.noreply.github.com>
2023-08-14 11:00:56 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,52 @@
+// Create web server
+// 1. Install express
+// 2. Import express
+// 3. Create application
+// 4. Run application
+
+// 1. Install express
+// npm install express
+// 2. Import express
+const express = require('express');
+const path = require('path');
+
+// 3. Create application
+const app = express();
+
+// 4. Run application
+// app.listen(3000, () => console.log('Server running on port 3000'));
+// app.listen(3000, () => console.log('Server running on port 3000'));
+// app.listen(3000, () => console.log('Server running on port 3000'));
+// app.listen(3000, () => console.log('Server running on port 3000'));
+// app.listen(3000, () => console.log('Server running on port 3000'));
+
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+// app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+// app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'about.html')));
+
+// app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'contact.html')));
+
+// app.get('/products', (req, res) => res.sendFile(path.join(__dirname, 'products.html')));
+
+// app.get('/products/:id', (req, res) => res.send(`Product ID: ${req.params.id}`));
+
+// app.get('/products/:id/:name', (req, res) => res.send(`Product ID: ${req.params.id} Product Name: ${req.params.name}`));
+
+// app.get('/products/:id/:name', (req, res) => {
+//     console.log(req.query);
+//     res.send(`Product ID: ${req.params.id} Product Name: ${req.params.name}`);
+// });
+
+// app.get('/products/:id/:name', (req, res) => {
+//     console.log(req.query);
+//     res.send(`Product ID: ${req.params.id} Product Name: ${req.params.name}`);
+// });
+
+// app.get('/products/:id/:name', (req, res) => {
+//     console.log(req.query);
+//     res.send(`Product ID: ${req.params.id} Product Name: ${req.params.name}`);
+// });
+
+// app.get('/products/:id/:name
